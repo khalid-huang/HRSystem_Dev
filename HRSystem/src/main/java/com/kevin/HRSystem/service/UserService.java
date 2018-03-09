0b6eaa0eed4c03668ae5a0d6824bceccf583cd48
sysuKinthon <qinkai1994@qq.com>
2018-03-09 21:16:47 +0800

    update

--- HRSystem/src/main/java/com/kevin/HRSystem/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.kevin.HRSystem.service;
-
-import com.kevin.HRSystem.model.User;
-
-public interface UserService {
-    public User selectUser(long userId);
-}
--- HRSystem/src/test/java/com/kevin/HRSystem/HRSystemApplicationTest.java
+++ HRSystem/src/test/java/com/kevin/HRSystem/HRSystemApplicationTest.java
@@ -3,25 +3,20 @@ package com.kevin.HRSystem;
 import com.kevin.HRSystem.dao.ApplicationDao;
 import com.kevin.HRSystem.dao.AttendDao;
 import com.kevin.HRSystem.dao.EmployeeDao;
-import com.kevin.HRSystem.dao.UserDao;
 import com.kevin.HRSystem.model.*;
 import com.kevin.HRSystem.service.EmpManagerService;
-import com.kevin.HRSystem.vo.AttendVo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring-mybatis.xml"})
 public class HRSystemApplicationTest {
 
-    @Autowired
-    private UserDao userDao;
 
     @Autowired
     private ApplicationDao applicationDao;
@@ -37,16 +32,6 @@ public class HRSystemApplicationTest {
 
     @Test
     public void testSelectUser() throws Exception {
-//        long id = 1;
-//        User user = userDao.selectUser(id);
-//        System.out.println(user);
-//
-//        User user1 = userDao.selectById(id);
-//        System.out.println(user1);
-//
-//        user1.setUsername("user");
-//        userDao.save(user1);
-
         //测试employee
 //        long e_id = 3;
 //        Employee employee = employeeDao.findById(e_id);
